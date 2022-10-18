35a74daee79d87cadabb47494b692ecd135867d5
berksoyyilmaz <berksoyyilmaz@gmail.com>
2022-10-18 21:56:50 +0300

    Design Patterns - Behavioral Patterns - Memento Pattern
    Signed-off-by: Berksoy Yılmaz <berksoy.yilmaz@etiya.com>

--- design-patterns/src/test/java/behavioral/memento/MementoTest.java
+++ design-patterns/src/test/java/behavioral/memento/MementoTest.java
@@ -4,15 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MementoTest {
-  @Test
-  void givenValue_whenSetState_thenReturnCurrentState() {
-    Originator originator = new Originator();
-
-    originator.setState("State #1");
-    originator.setState("State #2");
-
-    Assertions.assertEquals("State #2", originator.getState());
-  }
 
   @Test
   void givenValue_whenSetStateAndCareTakerAdd_thenReturnLastOverridedState() {
@@ -23,7 +14,13 @@ class MementoTest {
     originator.setState("State #2");
     careTaker.add(originator.saveStateToMemento());
 
+    originator.setState("State #3");
+    originator.setState("State #4");
+    careTaker.add(originator.saveStateToMemento());
+
     Assertions.assertEquals("State #2", careTaker.get(0).getState());
+    Assertions.assertEquals("State #4", careTaker.get(1).getState());
   }
 
 }
+
