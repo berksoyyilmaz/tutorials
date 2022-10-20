c82cb124ee0e6cdd23a147e2b80a6061cfc9d484
berksoyyilmaz <berksoyyilmaz@gmail.com>
2022-10-20 12:19:50 +0300

    Design Patterns - Behavioral Patterns - Template Method Pattern
    Signed-off-by: Berksoy Yılmaz <berksoy.yilmaz@etiya.com>

---
+++ design-patterns/src/main/java/behavioral/templatemethod/HighEndComputerBuilder.java
@@ -0,0 +1,21 @@
+package behavioral.templatemethod;
+
+public class HighEndComputerBuilder extends ComputerBuilder {
+
+  @Override
+  public void addMotherboard() {
+    computerParts.put("Motherboard", "High-end Motherboard");
+  }
+
+  @Override
+  public void setupMotherboard() {
+    motherboardSetupStatus.add("Screwing the high-end motherboard to the case.");
+    motherboardSetupStatus.add("Pluging in the power supply connectors.");
+    motherboardSetupStatus.forEach(System.out::println);
+  }
+
+  @Override
+  public void addProcessor() {
+    computerParts.put("Processor", "High-end Processor");
+  }
+}
---
+++ design-patterns/src/main/java/behavioral/templatemethod/StandardComputer.java
@@ -0,0 +1,10 @@
+package behavioral.templatemethod;
+
+import java.util.Map;
+
+public class StandardComputer extends Computer {
+
+  public StandardComputer(Map<String, String> computerParts) {
+    super(computerParts);
+  }
+}
