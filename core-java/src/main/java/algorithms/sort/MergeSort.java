a247b9bed0b891db85977bcddeb50ef818f59c26
berksoyyilmaz <berksoyyilmaz@gmail.com>
2022-11-08 00:31:13 +0300

    Java Data Structure and Algorithms - Merge Sort

---
+++ core-java/src/main/java/algorithms/sort/MergeSort.java
@@ -0,0 +1,81 @@
+package algorithms.sort;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class MergeSort {
+  public static void main(String[] args) {
+    Random random = new Random();
+    int[] numbers = new int[10];
+
+    for (int i = 0; i < numbers.length; i++) {
+      numbers[i] = random.nextInt(100);
+    }
+    System.out.print("Before: ");
+    printArray(numbers);
+    System.out.print("After: ");
+    mergeSort(numbers);
+    printArray(numbers);
+  }
+
+  private static void mergeSort(int[] inputArray) {
+    int inputLenght = inputArray.length;
+
+    if (inputLenght < 2) {
+      return;
+    }
+
+    int midIndex = inputLenght / 2;
+    int[] leftHalf = new int[midIndex];
+    int[] rightHalf = new int[inputLenght - midIndex];
+
+    for (int i = 0; i < midIndex; i++) {
+      leftHalf[i] = inputArray[i];
+    }
+
+    for (int i = midIndex; i < inputLenght; i++) {
+      rightHalf[i - midIndex] = inputArray[i];
+    }
+
+    mergeSort(leftHalf);
+    mergeSort(rightHalf);
+
+    merge(inputArray, leftHalf, rightHalf);
+  }
+
+  private static void merge(int[] inputArray, int[] leftHalf, int[] rightHalf) {
+    int leftSize = leftHalf.length;
+    int rightSize = rightHalf.length;
+
+    int i = 0, j = 0, k = 0;
+
+    while (i < leftSize && j < rightSize) {
+      if (leftHalf[i] <= rightHalf[j]) {
+        inputArray[k] = leftHalf[i];
+        i++;
+      } else {
+        inputArray[k] = rightHalf[j];
+        j++;
+      }
+      k++;
+    }
+
+    while (i < leftSize) {
+      inputArray[k] = leftHalf[i];
+      i++;
+      k++;
+    }
+
+    while (j < rightSize) {
+      inputArray[k] = rightHalf[j];
+      j++;
+      k++;
+    }
+  }
+
+  private static void printArray(int[] numbers) {
+    String collect = Arrays.stream(numbers).mapToObj(String::valueOf).collect(Collectors.joining(","));
+    System.out.println(collect);
+  }
+}
